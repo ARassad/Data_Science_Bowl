13191a7f63f1dfbc13f954e1e9d26c7d5e7a52be
Dmitriy <dmitribulankin@yandex.ru>
2018-02-07 16:02:39 +0300

    Begin proj

--- TEST.py
+++ TEST.py
@@ -93,3 +93,4 @@ sub['ImageId'] = new_test_ids
 sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
 sub.to_csv('sub-dsbowl2018-1.csv', index=False)
 
+#будьте любезны
