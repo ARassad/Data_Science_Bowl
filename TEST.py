460c23f9151ce7c9c687472bcb3cac4f1526049a
Arkadi <arassadnikov97@gmail.com>
2018-02-07 16:45:00 +0300

    First correct version

--- TEST.py
+++ TEST.py
@@ -2,7 +2,7 @@ import os
 import warnings
 
 import numpy as np
-
+import sys
 from tqdm import tqdm
 from skimage.io import imread, imsave
 from skimage.transform import resize
@@ -34,32 +34,35 @@ test_ids = next(os.walk(TEST_PATH))[1]
 
 # Get and resize train images and masks
 X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
-Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.uint8)
-X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
-sizes_test = []
-
-print('Getting and resizing images... ')
-for PATH, imgs, ids in [(TRAIN_PATH, X_train, train_ids), (TEST_PATH, X_test, test_ids)]:
-    for n, id_ in enumerate(ids):
-        path = PATH + id_
-        img = imread(path + '/images/' + id_ + '.png')[:, :, :IMG_CHANNELS]
-        sizes_test.append([img.shape[0], img.shape[1]])
-        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
-        imgs[n] = img
-
-print('Getting and resizing masks... ')
+Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+print('Getting and resizing train images and masks ... ')
+sys.stdout.flush()
 for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
-    path = TRAIN_PATH + id_ + '/masks/'
-    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.uint8)
-    ad = next(os.walk(path))[2]
-    for mask_file in next(os.walk(path))[2]:
-        if not mask_file.endswith('.png'):
-            continue
-        mask_ = imread(path + mask_file)
-        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH),
-                                      mode='constant', preserve_range=True), axis=-1)
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_train[n] = img
+    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                      preserve_range=True), axis=-1)
         mask = np.maximum(mask, mask_)
     Y_train[n] = mask
+
+# Get and resize test images
+X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+sizes_test = []
+print('Getting and resizing test images ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    path = TEST_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    sizes_test.append([img.shape[0], img.shape[1]])
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[n] = img
+
+print('Done!')
 
 
 # Predict on train, val and test
