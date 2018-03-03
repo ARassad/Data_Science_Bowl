fad314a37d5ffe782cf86b32ca5dfbfb076b3c78
Arkadi <arassadnikov97@gmail.com>
2018-03-03 11:51:53 +0300

    Добавлена функция получения картинок без клеток

--- detector/detector_data_prep.py
+++ detector/detector_data_prep.py
@@ -3,10 +3,14 @@ import os
 import tqdm
 import warnings
 
+import numpy as np
+
 PATH_FROM = "../../data/stage1_train/"
+PATH_TO = "../../data/detector/"
+PATH_TO_NON_NUCL = "../../data/detector_non/"
 
 
-def cut_nuc_from_image():
+def cut_nuclears():
     ids = next(os.walk(PATH_FROM))[1]
 
     for id_ in tqdm.tqdm(ids, total=len(ids)):
@@ -60,18 +64,65 @@ def cut_nuc_from_image():
                     break
 
             try:
-                if not os.path.isdir("../../data/detector/" + id_ + "/images/"):
-                    os.makedirs("../../data/detector/" + id_ + "/images/" )
-                imsave("../../data/detector/" + id_ + "/images/" + m_id, image[upper:bottom, left:right])
+                if not os.path.isdir(PATH_TO + id_ + "/images/"):
+                    os.makedirs(PATH_TO + id_ + "/images/" )
+                imsave(PATH_TO + id_ + "/images/" + m_id, image[upper:bottom, left:right])
 
-                if not os.path.isdir("../../data/detector/" + id_ + "/masks/"):
-                    os.makedirs("../../data/detector/" + id_ + "/masks/")
-                imsave("../../data/detector/" + id_ + "/masks/" + m_id, mask[upper:bottom, left:right])
+                if not os.path.isdir(PATH_TO + id_ + "/masks/"):
+                    os.makedirs(PATH_TO + id_ + "/masks/")
+                imsave(PATH_TO + id_ + "/masks/" + m_id, mask[upper:bottom, left:right])
             except:
                 print("EXCEPTION")
+
+
+def cut_non_nuclears(shape_win=(40, 40), strides=(40, 40), limit=(0.000, 0.02)):
+    ids = next(os.walk(PATH_FROM))[1]
+    id_im = 0
+    print("")
+    for id_ in tqdm.tqdm(ids, total=len(ids)):
+        path = PATH_FROM + id_
+        image = imread(path + "/images/" + id_ + ".png")[:, :, :3]
+
+        mask = np.zeros((image.shape[0], image.shape[1], 1), dtype=np.uint8)
+        for m_id in next(os.walk(path + "/masks/"))[2]:
+            if not m_id.endswith('.png'):
+                break
+            mask = np.maximum(mask, np.expand_dims(imread(path + "/masks/" + m_id)[:, :], axis=-1))
+
+        h_win, h_img, h_strd = shape_win[0], mask.shape[0], strides[0]
+        w_win, w_img, w_strd = shape_win[1], mask.shape[1], strides[1]
+        for h in range(h_win, h_img + h_strd, h_strd):
+            lower_bound = min(h, h_img - 1)
+            for w in range(w_win, w_img + w_strd, w_strd):
+                right_bound = min(w, w_img - 1)
+
+                part_mask = mask[lower_bound-h_win: lower_bound, right_bound-w_win: right_bound]
+
+                #  Сохранение маски и изображения если белых пикселей в маске немного
+                size = part_mask.shape[0] * part_mask.shape[1]
+                if size * limit[0] <= num_nuclea_pix(part_mask) <= size * limit[1]:
+                    id_im += 1
+                    if not os.path.isdir(PATH_TO_NON_NUCL + id_ + "/images/"):
+                        os.makedirs(PATH_TO_NON_NUCL + id_ + "/images/")
+                    imsave(PATH_TO_NON_NUCL + id_ + "/images/" + str(id_im) + ".png", image[lower_bound-h_win: lower_bound,
+                                                                                       right_bound-w_win: right_bound])
+                    if not os.path.isdir(PATH_TO_NON_NUCL + id_ + "/masks/"):
+                        os.makedirs(PATH_TO_NON_NUCL + id_ + "/masks/")
+                    imsave(PATH_TO_NON_NUCL + id_ + "/masks/" + str(id_im) + ".png", part_mask.reshape(shape_win))
+
+    return None
+
+
+def num_nuclea_pix(mask):
+    non_nucl_pix, num_nucl_pix = 0, 0
+
+    for pix in mask.flatten():
+        num_nucl_pix += int(pix != non_nucl_pix)
+
+    return num_nucl_pix
 
 
 if __name__ == "__main__":
     warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 
-    cut_nuc_from_image()
+    cut_non_nuclears()
