8f9df8b1c1083dda9cbfac760786abbcf83e2a98
Arkadi <arassadnikov97@gmail.com>
2018-03-22 19:33:35 +0300

    Оптимизирован алгоритм (для шага (4,4) улучшилось с 22 часов до 30 минут)

--- detector/prediction.py
+++ detector/prediction.py
@@ -17,10 +17,13 @@ import matplotlib.patches as patches
 
 STRIDES = (4, 4)  # Шаги с которыми идет окошко детектора
 COEF_RES = 1.3  # Коэффицент с которым уменьшаеться размер картинки
-MIN_RESIZE = 4  #  Коэф минимального размера картинки
+MIN_RESIZE = 4  # Коэф минимального размера картинки
 MAX_RESIZE = 2
 
 SIZE_IMAGE = (32, 32)
+
+TRESHHOLD_FIRST_DETECTOR = 0.99
+TRESHHOLD_SECOND_DETECTOR = 0.95
 
 
 def copy_arr_to_arr(to, from_, mode="mean"):
@@ -458,8 +461,156 @@ def main_merge_with_NMS():
     sub.to_csv('detector_Unet.csv', index=False)
 
 
+def main_merge_with_NMS_optimizer():
+    warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+
+    new_test_ids = []
+    rles = []
+
+    detector = None
+    detector_second = None
+    with CustomObjectScope({'relu6': applications.mobilenet.relu6,
+                            'DepthwiseConv2D': applications.mobilenet.DepthwiseConv2D}):
+        detector = load_model('detector_MobileNet.h5')
+        detector_second = load_model('detector_MobileNet_Second.h5')
+    u_net = load_model("U-net/Unet(32x32).h5", custom_objects={'mean_iou': mean_iou})
+
+    dir_ = "../../data/stage1_test/"
+    #dir_ = "../../data/1/"
+    ids_test = os.walk(dir_)
+    ids = next(ids_test)[1]
+    for n, id_ in tqdm(enumerate(ids), total=len(ids)):
+        try:
+            path = dir_ + id_ + "/images/"
+            image = imread(path + id_ + ".png", as_grey=True)
+
+            images = []
+            masks = []
+
+            bounding_boxs = []
+            bounding_boxs_nms = []
+
+            # Создаем разные размеры исходной картинки
+            cur_size = (int(image.shape[0] * MAX_RESIZE), int(image.shape[1] * MAX_RESIZE))
+            min_size = (image.shape[0] // MIN_RESIZE, image.shape[1] // MIN_RESIZE)
+
+            images.append(image)
+            masks.append(np.zeros(image.shape, dtype=np.float64))
+            while True:
+                images.append(resize(image, cur_size, mode='constant', preserve_range=True))
+                masks.append(np.zeros(cur_size, dtype=np.float64))
+
+                cur_size = (int(cur_size[0] // COEF_RES), int(cur_size[1] // COEF_RES))
+                if cur_size[0] <= min_size[0] or cur_size[1] <= min_size[1]:
+                    break
+
+            # Обрабатываем каждую получившуюся картинку
+            for i in range(len(images)):
+                im = images[i]
+
+                # Вырезаем из картинки квадратики для детектора
+
+                square_for_detector = []
+                bounds = []
+                bbs = []
+
+                for h in range(SIZE_IMAGE[0], im.shape[0] + STRIDES[0], STRIDES[0]):
+                    lower_bound = min(h, im.shape[0])
+
+                    for w in range(SIZE_IMAGE[1], im.shape[1] + STRIDES[1], STRIDES[1]):
+                        right_bound = min(w, im.shape[1])
+
+                        bounds.append((right_bound, lower_bound))
+                        square_for_detector.append(im[lower_bound - SIZE_IMAGE[0]: lower_bound,
+                                                           right_bound - SIZE_IMAGE[1]: right_bound]\
+                                                           .reshape((SIZE_IMAGE[0], SIZE_IMAGE[1], 1)))
+
+                square_for_detector = np.array(square_for_detector)
+
+                # Говорит что в квадрате есть клетки
+                is_nucl = detector.predict(square_for_detector)
+                square_for_detector = np.array([square_for_detector[i] for i in range(len(square_for_detector))
+                                                  if is_nucl[i][0] > TRESHHOLD_FIRST_DETECTOR])
+                bounds = [bounds[i] for i in range(len(bounds)) if is_nucl[i][0] > TRESHHOLD_FIRST_DETECTOR]
+
+                # Говорит, что в квадрате только одна клетка
+                if len(square_for_detector) > 0:
+                    is_one_nucl = detector_second.predict(square_for_detector)
+
+                    bounds = [bounds[i] for i in range(len(bounds)) if is_one_nucl[i][0] > TRESHHOLD_SECOND_DETECTOR]
+                    for box in bounds:
+                        bbs.append((box[0] - SIZE_IMAGE[0], box[1] - SIZE_IMAGE[1], box[0], box[1]))
+
+                bounding_boxs.append(bbs)
+                bounding_boxs_nms.append(non_max_suppression_fast(np.array(bbs), 0.6))
+
+            result_mask = np.zeros(image.shape, dtype=np.float64)
+            result_bbs = []
+
+            # Мержим все боксы
+            for i in range(len(images)):
+                h_coef = images[i].shape[0] / image.shape[0]
+                w_coef = images[i].shape[1] / image.shape[1]
+                for bb in bounding_boxs_nms[i]:
+                    box = (bb[0] // w_coef,
+                           bb[1] // h_coef,
+                           bb[2] // w_coef,
+                           bb[3] // h_coef)
+                    result_bbs.append(box)
+
+            result_nms = non_max_suppression_fast(np.array(result_bbs), 0.5)
+
+            # Предсказание по NMS
+            for box in result_nms:
+                im_to_unet = np.ndarray((1, SIZE_IMAGE[0], SIZE_IMAGE[1], 1), dtype=np.float64)
+
+                # Приводим размеры квадрата к размерам входа Юнет
+                im_to_unet[0] = resize(image[box[1]:box[3], box[0]:box[2]], SIZE_IMAGE,
+                                       mode='constant', preserve_range=True).reshape((SIZE_IMAGE[0], SIZE_IMAGE[1], 1))
+                # Предсказывание
+                pred_mask = u_net.predict(im_to_unet)[0]
+
+                # Возвращаем начальный размер и вставляем в итоговую маску
+                copy_arr_to_arr(result_mask[box[1]:box[3], box[0]:box[2]],
+                                resize(pred_mask, (box[3] - box[1], box[2] - box[0]), mode='constant', preserve_range=True),
+                                "mean_except_zero")
+
+            # Кодирование
+            mask_to_encde = result_mask
+
+            rle = list(prob_to_rles(mask_to_encde, cutoff=0.5))
+            rles.extend(rle)
+            new_test_ids.extend([id_] * len(rle))
+            if len(rle) == 0:
+                rles.extend([[1, 1]])
+                new_test_ids.extend([id_])
+
+            save_result("../../data/detector_unet_pred/{}.png".format(id_), image, mask_to_encde, images,
+                        bounding_boxs, bounding_boxs_nms, result_bbs, result_nms)
+
+        except StopIteration:
+            print("Exception id: " + id_)
+
+    # Create submission DataFrame
+    sub = pd.DataFrame()
+    sub['ImageId'] = new_test_ids
+    sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
+
+    # Чистка от мусора
+    def f(x):
+        res = 0
+        for i in range(1, len(x), 2):
+            res += i
+        return res
+
+    sub['CountPix'] = pd.Series(rles).apply(f)
+    sub = sub[sub['CountPix'] > 10][['ImageId', 'EncodedPixels']]
+
+    sub.to_csv('detector_Unet.csv', index=False)
+
+
 if __name__ == "__main__":
-    main_merge_with_NMS()
+    main_merge_with_NMS_optimizer()
 
 
 
